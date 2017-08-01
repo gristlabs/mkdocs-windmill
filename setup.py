52f4fa5fa9fe17e9ada5e9e2ff6ad1e42fb83901
Nguyen Van Bach <vanbach1213@gmail.com>
2017-08-01 22:03:17 +0900

    Change package info

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 setup(
-    name="mkdocs-windmill",
+    name="mkdocs-windmillex",
     version=VERSION,
-    url='https://github.com/gristlabs/mkdocs-windmill',
+    url='https://github.com/guenbakku/mkdocs-windmillex',
     license='MIT',
     description='MkDocs theme focused on navigation and usability',
-    author='Dmitry S',
-    author_email='dmitry@getgrist.com',
+    author='',
+    author_email='',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
         'mkdocs.themes': [
-            'windmill = mkdocs_windmill',
+            'windmillex = mkdocs_windmillex',
         ]
     },
     zip_safe=False
